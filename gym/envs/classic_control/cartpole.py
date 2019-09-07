f76ba5e1477ad59432077c8598bd7d0332dece53
mmhc1710 <mmhc@ku.edu>
2019-09-07 14:46:36 -0500

    -

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -91,9 +91,10 @@ class CartPoleEnv(gym.Env):
         return np.array(self.state), reward, done, {}
 
     def _reset(self):
-        #self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        self.state = self.np_random.uniform(low=-0.25, high=0.25, size=(4,))
-        self.steps_beyond_done = None
+        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
+        #self.state = self.np_random.uniform(low=-0.10, high=0.10, size=(4,))
+	#self.state = np.array([0.0,0.0,-0.7,0.0])       
+	self.steps_beyond_done = None
         return np.array(self.state)
 
     def _render(self, mode='human', close=False):
